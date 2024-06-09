15b94ccedaad8580db3b5f16bc18564fb49932f4
hossainchisty <hossain.chisty11@gmail.com>
2024-06-09 13:34:03 +0600

    fix: Swagger UI on prod

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -43,4 +45,4 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("", redirect_to_swagger),  # Redirect root URL to Swagger UI
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
